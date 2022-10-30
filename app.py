a1997e738c51731eff3d8eb52dea396bee542348
Arjun47 <singhaniya97arjun@gmail.com>
2022-10-30 23:03:57 +0530

    added TODO items

--- app.py
+++ app.py
@@ -3,5 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route("/new", methods=['POST'])
+def add_record():
+    # TODO: an endpoint to add new movie/show review
+    pass
+
+@app.route("/delete/<int:id>")
+def delete_record(id):
+    # TODO: an endpoint to delete a particular movie/show
+    pass
+
+@app.route("/update/<int:id>")
+def update_record(id):
+    # TODO: an endpoint to update the existing movie/show record
+    pass
+
+@app.route("/get/<int:id>", methods=['GET'])
+def get_record(id):
+    # TODO: an endpoint to fetch available records
+    pass
+
